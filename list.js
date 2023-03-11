39545233b2ed8f36ce13d03e705011e40bfc9e9e
naviduNavoda <127333117+naviduNavoda@users.noreply.github.com>
2023-03-11 11:36:46 +0530

    1.1

---
+++ list.js
@@ -0,0 +1,85 @@
+
+var foods_breakfast= [
+    {
+        name : "a",
+        price : 3400,
+        img : "foodImgs/k.jpg",
+        offer : 0
+    },
+    {
+        name : "b",
+        price : 3490,
+        img : "foodImgs/b.jpg",
+        offer : 10
+    },
+
+
+];
+
+var foods_lunch = [
+    {
+        name : "a",
+        price : 3400,
+        img : "foodImgs/k.jpg",
+        offer : 0
+    },
+    {
+        name : "b",
+        price : 3490,
+        img : "foodImgs/b.jpg",
+        offer : 10
+    },
+
+
+];
+
+var foods_dinner = [
+    {
+        name : "a",
+        price : 3400,
+        img : "foodImgs/k.jpg",
+        offer : 0
+    },
+    {
+        name : "b",
+        price : 3490,
+        img : "foodImgs/b.jpg",
+        offer : 10
+    },
+
+
+];
+
+var foods_dessert = [
+    {
+        name : "a",
+        price : 3400,
+        img : "foodImgs/k.jpg",
+        offer : 0
+    },
+    {
+        name : "b",
+        price : 3490,
+        img : "foodImgs/b.jpg",
+        offer : 10
+    },
+
+
+];
+
+var foods_shake = [
+    {
+        name : "a",
+        price : 3400,
+        img : "foodImgs/k.jpg",
+        offer : 0
+    },
+    {
+        name : "b",
+        price : 3490,
+        img : "foodImgs/b.jpg",
+        offer : 10
+    },
+
+
+];
